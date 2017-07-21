82f0e0e3d3aff776f31b02784cb928db97c7d893
Nadya Prabaningrum <nadya.prabaningrum@mail.ugm.ac.id>
2017-07-21 17:21:46 +0700

    Update register.blade.php

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,117 +1,102 @@
 @extends('layouts.app')
-
 @section('content')
 <div class="container">
-	<div class="row" style="margin-top: 30px; margin-bottom: 40px">
-		<div class="col-lg-12">
-		<!-- top -->
-		<h1>Buat Akun Baru</h1>
+  <div class="col-md-8">
+      <h4>Buat Akun Baru</h4>
+    <div class="primary-blue line margin5-top"></div>
+    <div class="panel panel-default radius">
+      <div class="panel-body">
+          <div class="col-md-12">
+            <div class="form-group">
+              <span>Email</span>
+              <input type="text" class="form-control radius" id="usr" placeholder="ex: mail@gmail.com"/>
+            </div>
+          </div>
+          <div class="col-md-12">
+            <div class="form-group">
+              <span>Password</span>
+              <input type="password" id="password" name="password" class="form-control radius" data-toggle="password">
+            </div>
+          </div>
+          <div class="col-md-12">
+            <span>Masukkan Kode Captcha</span>
+						<span>{!! captcha_image_html('ContactCaptcha') !!}</span>
+				    <input type="text" class="form-control radius" style="margin-top:1%" id="inputCapcha">
+          </div>
+          <div class="col-md-12 margin5-top">
+            <input type="checkbox" name="" value="">
+            <span>Saya setuju dengan</span><label>&nbspSyarat dan Ketentuan</label>
+          </div>
+      </div>
+    </div><!-- ct panel -->
+    <button type="button" class="btn btn-primary-yel" style="width:100%" name="button">Daftar</button>
+		<div class="row margin5">
+			<!-- <hr>	<center>Atau</center><hr> -->
+			<center>
+			<div class="col-md-5" style="height:1px; background-color:#000; margin-top:1%">
+
+			</div>
+			<div class="col-md-2">
+				Atau
+			</div>
+			<div class="col-md-5" style="height:1px; background-color:#000; margin-top:1%">
+
+			</div>
+			</center>
 		</div>
-		<div class="col-lg-7">
-		<!-- div form sign up -->
-			<form>
-			<div class="panel panel-primary">
-			<!-- bold line -->
-				<div class="panel-heading" style="padding-bottom: 0"></div>
-				<div class="panel-body">
-				<!-- form -->
-					<div class="form-group">
-					<!-- email -->
-						<label>Email</label>
-						<input type="email" class="form-control" name="email" id="inputEmail" placeholder="Email. ex : mail@email.com">
-					</div>
-					<div class="form-group">
-					<!-- password -->
-						<label for="inputPassword">Password</label>
-				        <div class="input-group">
-				          <input type="password" id="value-pass" class="form-control" placeholder="password">
-				          <span class="input-group-btn">
-				            <button id="show-pass" class="btn btn-default reveal" type="button"><i class="fa fa-eye"></i></button>
-				          </span>
-				        </div>
-					</div>
-					<div class="form-group">
-					<!-- captcha -->
-					    <label>Captcha</label>
-					    {!! captcha_image_html('ContactCaptcha') !!}
-					    <input type="text" class="form-control" id="inputCapcha" placeholder="Masukan Capcha">
-					</div>
-				</div>
+		<div class="row margin5">
+			<div class="col-md-6">
+					<button type="button" class="btn btn-primary" style="width:100%" name="button"><i class="fa fa-facebook-official"></i>&nbspDaftar dengan Facebook</button>
 			</div>
-			<div class="form-group">
-			<!-- submit -->
-			  <center>
-			    <input type="submit" class="btn btn-warning btn-block btn-submit-bold" name="signup" value="Daftar">
-			  </center>
-			</div>
-			</form>
-
-			<!-- straight line -->
-			<div class="col-lg-5 str-line">
-			</div>
-
-			<div class="col-lg-2">
-			<center>Atau</center>
-			</div>
-
-			<!-- straight line -->
-			<div class="col-lg-5 str-line">
-			</div>
-			<div style="margin-top: 8%">
-			<!-- sign in with -->
-			<div class="col-lg-6">
-			<!-- button g+ -->
-			<a class="btn btn-danger btn-block"><i class="fa fa-google-plus"></i> Sign in with Google</a>
-			</div>
-			<div class="col-lg-6">
-			<!-- button facebook -->
-			<a class="btn btn-primary btn-block"><i class="fa fa-facebook-official"></i> Log in with Facebook</a>
-			</div>
+			<div class="col-md-6">
+					<button type="button" class="btn btn-danger" style="width:100%" name="button"><i class="fa fa-google-plus"></i>&nbspDaftar dengan Google</button>
 			</div>
 		</div>
+  </div><!-- ct col-md-8 -->
 
-		<div class="col-lg-5">
-			<div class="col-lg-12">
-				<div class="col-lg-4">
-				<!-- image icon -->
-				<center>
-				<i class="fa fa-vcard logo-icon" style="padding: 20px 0"></i>
-				</center>
-				</div>
-				<div class="col-lg-8">
-				<!-- detail -->
-					<div class="col-lg-12">
-					<!-- title -->
-					<h4>HOT WALLET</h4>
-					</div>
-					<div class="col-lg-12">
-					<!-- description -->
-					Anda akan mendapatkan kenyamanan berupa hot wallet ketika memiliki akun di citos.id.
-					Apa itu hot wallet? cek disini.
-					</div>
-				</div>
+  <!-- Right Side -->
+  <div class="col-md-4 margin5-top">
+    <div class="row margin5-top">
+      <center>
+    	<div class="col-md-5">
+        <div class="circle primary-blue" style="border-radius:70%; height:70px; width:70px">
+          <img src="/img/ic-poin.png" style="margin-top:15%">
+        </div>
+    	</div>
+      </center>
+			<div class="col-md-7">
+				<strong style="color:#0033a8">Dapatkan Poin</strong><br>
+				<span class="small">Dapatkan poin setiap melakukan transaksi dengan memiliki akun</span>
 			</div>
-			<div class="col-lg-12" style="margin-top: 5%">
-				<div class="col-lg-4">
-				<!-- image icon -->
-				<center>
-				<i class="fa fa-paper-plane logo-icon"></i>
-				</center>
-				</div>
-				<div class="col-lg-8">
-				<!-- detail -->
-					<div class="col-lg-12">
-					<!-- title -->
-					<h4>POIN</h4>
-					</div>
-					<div class="col-lg-12">
-					<!-- description -->
-					Dapatkan poin setiap melakukan transaksi dengan memiliki akun.
-					</div>
-				</div>
+    </div><br><br>
+		<div class="row margin5-top">
+      <center>
+    	<div class="col-md-5">
+        <div class="circle primary-blue" style="border-radius:70%; height:70px; width:70px">
+          <img src="/img/ic-wallet.png" style="margin-top:15%">
+        </div>
+    	</div>
+      </center>
+			<div class="col-md-7">
+				<strong style="color:#0033a8">Hot Wallet</strong><br>
+				<span class="small">Anda akan mendapatkan kenyamanan berupa hot wallet ketika memiliki akun di citos.id</span><br>
+				<a class="small" href="#">Apa itu Hot Wallet?</a>
 			</div>
-		</div>
-	</div>
+    </div><br><br>
+		<div class="row margin5-top">
+      <center>
+    	<div class="col-md-5">
+        <div class="circle primary-blue" style="border-radius:70%; height:70px; width:70px">
+          <img src="/img/ic-time.png" style="margin-top:15%">
+        </div>
+    	</div>
+      </center>
+			<div class="col-md-7">
+				<strong style="color:#0033a8">Cepat dan Efisien</strong><br>
+				<span class="small">Dengan memiliki akun, Anda dapat memilih tiket dengan cepat</span>
+			</div>
+    </div>
+  </div>
 </div>
 
 @endsection
