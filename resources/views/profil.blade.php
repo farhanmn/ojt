1c73224db660021dd2df5156ce99151fd63304f8
Nadya Prabaningrum <nadya.prabaningrum@mail.ugm.ac.id>
2017-07-22 10:10:19 +0700

    Update profil.blade.php

--- resources/views/profil.blade.php
+++ resources/views/profil.blade.php
@@ -2,100 +2,75 @@
 @section('content')
 <div class="container margin5-top">
   <div class="col-md-8">
-    <div class="test"><h4 style="padding: 2% 0 2% 7%; margin: 0;color: #fff">Buat Akun Baru</h4></div>
+      <div class="test"><h4 style="padding: 2% 0 2% 7%; margin: 0;color: #fff">Informasi Personal</h4></div>
     <div class="panel panel-default radius">
       <div class="panel-body">
+          <div class="col-md-2">
+            <div class="form-group">
+              <span>Gelar</span>
+              <div class="form-inline">
+                <select class="form-control btn-block radius">
+                  <option value="Tuan">Tuan</option>
+                  <option value="Nyonya">Nyonya</option>
+                  <option value="Nona">Nona</option>
+                </select>
+              </div>
+            </div>
+          </div> <!-- ct col md-4 -->
+          <div class="col-md-10">
+            <div class="form-group">
+              <span>Nama Lengkap</span>
+              <input type="text" class="form-control radius" id="usr"/>
+            </div>
+          </div>
+          <!-- ct col-md-8 -->
           <div class="col-md-12">
             <div class="form-group">
-              <span>Email</span>
-              <input type="text" class="form-control radius" id="usr" placeholder="ex: mail@gmail.com"/>
+              <span>Tanggal Lahir</span>
+              <input type="text" class="form-control radius" id="usr"/>
             </div>
           </div>
           <div class="col-md-12">
-            <div class="form-group">
-              <span>Password</span>
-              <input type="password" id="password" name="password" class="form-control radius" data-toggle="password">
-            </div>
+            <span>No Telepon</span>
           </div>
           <div class="col-md-12">
-            <span>Masukkan Kode Captcha</span>
-						<span>{!! captcha_image_html('ContactCaptcha') !!}</span>
-				    <input type="text" class="form-control radius" style="margin-top:1%" id="inputCapcha">
+            <div class="form-group">
+              <input class="form-control radius" type="tel" id="phone">
+            </div>
+          </div> <!-- ct col md-4 -->
+          <div class="col-md-12">
+            <span>Negara</span>
+          </div>
+          <div class="col-md-12">
+            <select class="form-control radius" id="country" name ="country"></select>
           </div>
           <div class="col-md-12 margin5-top">
-            <input type="checkbox" name="" value="">
-            <span>Saya setuju dengan</span><label>&nbspSyarat dan Ketentuan</label>
+            <span>Negara Bagian / Provinsi</span>
+          </div>
+          <div class="col-md-12">
+            <select class="form-control radius" name ="state" id ="state"></select>
+          </div>
+          <div class="col-md-12 margin5-top">
+            <div class="form-group">
+              <span>Kota</span>
+              <input type="text" class="form-control radius" id="usr"/>
+            </div>
           </div>
       </div>
     </div><!-- ct panel -->
-    <button type="button" class="btn btn-primary-yel" style="width:100%" name="button">Daftar</button>
-		<div class="row margin5">
-			<!-- <hr>	<center>Atau</center><hr> -->
-			<center>
-			<div class="col-md-5" style="height:1px; background-color:#000; margin-top:1%">
-
-			</div>
-			<div class="col-md-2">
-				Atau
-			</div>
-			<div class="col-md-5" style="height:1px; background-color:#000; margin-top:1%">
-
-			</div>
-			</center>
-		</div>
-		<div class="row margin5">
-			<div class="col-md-6">
-					<button type="button" class="btn btn-primary" style="width:100%" name="button"><i class="fa fa-facebook-official"></i>&nbspDaftar dengan Facebook</button>
-			</div>
-			<div class="col-md-6">
-					<button type="button" class="btn btn-danger" style="width:100%" name="button"><i class="fa fa-google-plus"></i>&nbspDaftar dengan Google</button>
-			</div>
-		</div>
+    <button type="button" class="btn btn-primary-yel" style="width:100%; margin-bottom:30%" name="button">Simpan</button>
   </div><!-- ct col-md-8 -->
-
-  <!-- Right Side -->
+  <!-- change photo profil -->
   <div class="col-md-4 margin5-top">
-    <div class="row margin5-top">
-      <center>
-    	<div class="col-md-5">
-        <div class="circle primary-blue" style="border-radius:70%; height:70px; width:70px">
-          <img src="/img/ic-poin.png" style="margin-top:15%">
-        </div>
-    	</div>
-      </center>
-			<div class="col-md-7">
-				<strong style="color:#0033a8">Dapatkan Poin</strong><br>
-				<span class="small">Dapatkan poin setiap melakukan transaksi dengan memiliki akun</span>
-			</div>
-    </div><br><br>
-		<div class="row margin5-top">
-      <center>
-    	<div class="col-md-5">
-        <div class="circle primary-blue" style="border-radius:70%; height:70px; width:70px">
-          <img src="/img/ic-wallet.png" style="margin-top:15%">
-        </div>
-    	</div>
-      </center>
-			<div class="col-md-7">
-				<strong style="color:#0033a8">Hot Wallet</strong><br>
-				<span class="small">Anda akan mendapatkan kenyamanan berupa hot wallet ketika memiliki akun di citos.id</span><br>
-				<a class="small" href="#">Apa itu Hot Wallet?</a>
-			</div>
-    </div><br><br>
-		<div class="row margin5-top">
-      <center>
-    	<div class="col-md-5">
-        <div class="circle primary-blue" style="border-radius:70%; height:70px; width:70px">
-          <img src="/img/ic-time.png" style="margin-top:15%">
-        </div>
-    	</div>
-      </center>
-			<div class="col-md-7">
-				<strong style="color:#0033a8">Cepat dan Efisien</strong><br>
-				<span class="small">Dengan memiliki akun, Anda dapat memilih tiket dengan cepat</span>
-			</div>
+    <center>
+    <div class="margin5">
+      <img id='img-upload' class="foto" src="/img/sponge.jpg"/>
     </div>
+
+    <button type="button" class="btn btn-primary-blue btn-file btn-block" style="width:75%" name="button">
+      Unggah Foto Profil <input type="file" id="imgInp">
+    </button>
+    </center>
   </div>
 </div>
-
-@endsection
+@stop
